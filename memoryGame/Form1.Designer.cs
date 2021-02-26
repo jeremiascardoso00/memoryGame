001eeec70f291d428d8a7d789fe635036165b57a
jeremias cardoso <jeremiascardoso00@outlook.com>
2021-02-26 09:03:16 -0300

    Agregar archivos de proyecto.

---
+++ memoryGame/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace memoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "btn1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(201, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "btn2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 87);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "btn3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(201, 105);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 87);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "btn4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(12, 198);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(183, 87);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "btn5";
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(201, 198);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(183, 87);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "btn6";
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(12, 291);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(183, 87);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "btn7";
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(201, 291);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(183, 87);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "btn8";
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(12, 384);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(183, 87);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "btn9";
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.White;
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(201, 384);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(183, 87);
+            this.button10.TabIndex = 9;
+            this.button10.Tag = "btn10";
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(162, 561);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Tag = "inciar_btn";
+            this.button11.Text = "Iniciar";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(395, 596);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ memoryGame/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memoryGame
+{
+    public partial class Form1 : Form
+    {
+        int[] arr = new int[10];
+        private Button[] buttons;
+        private Element[] elements = new Element[10];
+        int lastChoice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async private void gameStart()
+        {
+            buttons = new Button[] { button1, button2, button3, button4, button5, button6, button7, button8, button9, button10 };
+            this.lastChoice = -1;
+            resetGame();
+            int cont = 0;
+            Random r = new Random();
+            while (cont < 10)
+            {
+                this.arr[cont] = r.Next(0, 10);
+                if (arr.Count(x => x == arr[cont]) <= 1 || cont == 0)
+                {
+                    cont++;
+                }
+            }
+
+            setButtons();
+            desactivateButtons();
+            await Task.Delay(3000);
+            resetButtons();
+            activateButtons();
+        }
+
+        private void setButtons()
+        {
+            Color[] color = { Color.Red, Color.Red, Color.Blue, Color.Blue, Color.Green, Color.Green, Color.Yellow, Color.Yellow, Color.Brown, Color.Brown };
+            int cont = 0;
+            for(int i=0; i<10; i++)
+            {
+                this.buttons[this.arr[i]].BackColor = color[i];
+                if (cont == 0)
+                {
+                    elements[this.arr[i]] = new Element(buttons[i], color[i], i, i + 1);
+                    cont++;
+                }
+                else if(cont >= 1)
+                {
+                    elements[this.arr[i]] = new Element(buttons[i], color[i], i, i - 1);
+                    cont = 0;
+                }
+            }
+        }
+
+        private void resetButtons()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                this.buttons[i].BackColor = Color.White;
+            }
+        }
+
+        public void activateButtons()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (this.buttons[i].BackColor == Color.White)
+                {
+                    this.buttons[i].Enabled = true;
+                }
+            }
+        }
+
+        public void desactivateButtons()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (this.buttons[i].BackColor == Color.White)
+                {
+                    this.buttons[i].Enabled = false;
+                }
+            }
+        }
+
+        async public void checkUserEvent(int actualChoice)
+        {
+            bool badChoice = false;
+            
+            desactivateButtons();
+            if ( this.lastChoice != -1)
+            {
+                Refresh();
+                Color actualColor = elements[actualChoice].Color;
+                Color lastColor = elements[lastChoice].Color;
+                if (actualColor == lastColor)
+                {
+                    buttons[actualChoice].BackColor = elements[actualChoice].Color;
+                    this.buttons[actualChoice].Enabled = false;
+                    Console.WriteLine("great!");
+                    this.lastChoice = -1;
+                }
+                else
+                {
+                    Console.WriteLine("grong");
+                    resetGame();
+                    badChoice = true;
+                }
+            }
+            else
+            {
+                lastChoice = actualChoice;
+                buttons[actualChoice].BackColor = elements[actualChoice].Color;
+                this.buttons[actualChoice].Enabled = false;
+            }
+            await Task.Delay(500);
+            if (!badChoice) activateButtons();
+        }
+        private void resetGame()
+        {
+            resetButtons();
+            desactivateButtons();
+            this.arr = new int[10];
+            this.elements = new Element[10];
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            gameStart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(4);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(5);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(6);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(7);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(8);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            checkUserEvent(9);
+        }
+    }
+
+    public class Element
+    {
+        Button _button;
+        Color _color;
+        int _ownPosition;
+        int _parPosition;
+       
+        public int OwnPosition { get => _ownPosition; set => _ownPosition = value; }
+        public int ParPosition { get => _parPosition; set => _parPosition = value; }
+        public Button Button { get => _button; set => _button = value; }
+        public Color Color { get => _color; set => _color = value; }
+
+        public Element(Button button, Color color, int ownPosition, int parPosition)
+        {
+            this._button = button;
+            this._color = color;
+            this._ownPosition = ownPosition;
+            this._parPosition = parPosition;
+        }
+    }
+}
